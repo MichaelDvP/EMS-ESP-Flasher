84238eabf6a74cc6f5f29a31284e1cd13f120478
MichaelDvP <github@mdvp.de>
2024-03-13 12:29:07 +0100

    path to my_repo

--- esp_flasher/const.py
+++ esp_flasher/const.py
@@ -3,13 +3,13 @@ import re
 __version__ = "1.0.2"
 
 
-ESP32_DEFAULT_OTA_DATA = "https://github.com/espressif/arduino-esp32/raw/2.0.5/tools/partitions/boot_app0.bin"
+ESP32_DEFAULT_OTA_DATA = "https://github.com/MichaelDvP/EMS-ESP-Flasher/raw/my_repo/bin/boot_app0.bin"
 
 ESP32_DEFAULT_BOOTLOADER_FORMAT = (
-    "https://github.com/espressif/arduino-esp32/raw/2.0.5/"
-    "tools/sdk/$MODEL$/bin/bootloader_$FLASH_MODE$_$FLASH_FREQ$.bin"
+    "https://github.com/MichaelDvP/EMS-ESP-Flasher/raw/my_repo/"
+    "bin/$MODEL$/bootloader_$FLASH_MODE$_$FLASH_FREQ$.bin"
 )
-ESP32_DEFAULT_PARTITIONS =  "https://github.com/MichaelDvP/EMS-ESP-Flasher/raw/main/partitions/partitions_$MODEL$_$FLASH$.bin"
+ESP32_DEFAULT_PARTITIONS =  "https://github.com/MichaelDvP/EMS-ESP-Flasher/raw/my_repo/partitions/partitions_$MODEL$_$FLASH$.bin"
 
 
 # https://stackoverflow.com/a/3809435/8924614
